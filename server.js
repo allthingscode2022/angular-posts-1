4d0af9ffbeb2ec50a6442843c5a7f72f73eddfe6
Carlos Coello <carlos.coello05@outlook.com>
2019-02-17 22:46:07 -0600

    updated file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(
 );
 
 // middleware
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "/dist/posts")));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb", extended: true }));
 
